56ca82114a9fdbbc95c4b65a4b83995a3dc15813
Max-500 <213497@ids.upchiapas.edu.mx>
2023-01-24 20:32:50 -0600

    Mejora del Servidor

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,3 @@
-const { read } = require('fs');
 const { Socket } = require('net');
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -6,17 +5,22 @@ const readline = require('readline').createInterface({
 })
 
 const socket = new Socket();
-
 socket.connect({ host:'127.0.0.1', port:3000 })
-
 socket.setEncoding("utf-8")
 
-socket.write("Hola")
+//socket.write("Hola")
 
-readline.on("data", (line) =>{
+readline.on("line", (line) =>{
     socket.write(line)
+    if(line == 'END'){
+        socket.end()
+    }
 })
 
 socket.on("data", (data) => {
     console.log(data)
+})
+
+socket.on("close", ()=>{
+    process.exit(0)
 })
--- Server.js
+++ Server.js
@@ -1,23 +1,56 @@
-const { Server } = require('net');
+const { Server } = require("net");
 
 const server = new Server();
 
-//Significa que alguien se conecto al server y sacamos su IP y puerto
-server.on("connection", (socket) => {
-    //Se puso porque como tal en el socket mandamos bits y esos los tenemos que poner en utf-8
-    socket.setEncoding("utf-8")
+function error(err) {
+  console.error(err);
+  process.exit(1);
+}
 
-    console.log(`New Connection from ${socket.remoteAddress}:${socket.remotePort}`);
+function start(port) {
+  //Significa que alguien se conecto al server y sacamos su IP y puerto
+  server.on("connection", (socket) => {
+    //Se puso porque como tal en el socket mandamos bits y esos los tenemos que poner en utf-8
+    socket.setEncoding("utf-8");
+
+    const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
+
+    console.log(`New Connection from ${remoteSocket}`);
 
     //Este va a ser cuando tenga que leerse algo de información del socket
     socket.on("data", (data) => {
-        console.log(data)
-        //Mandamos el mismo mensaje al cliente
-        socket.write("Reenviando mensaje del cliente desde el servidor al cliente " + data);
-    })
-})
+      if (data == "END") {
+        socket.end();
+      } else {
+        socket.write(
+          "Reenviando mensaje del cliente desde el servidor al cliente " + data
+        );
+      }
+    });
+  });
 
-server.listen({host:'0.0.0.0', port: 3000}, ()=>{
-    console.log("Server is RUNNING IN PORT 3000")
-}) 
+  server.listen({ host: "0.0.0.0", port: port }, () => {
+    console.log(`Server is RUNNING IN PORT ${port}`);
+  });
 
+  server.on("error", (err)=>{
+    error(err.message);
+  })
+}
+
+const main = () => {
+  if (process.argv.length !== 3) {
+    error(`Ingresa lo siguiente \n node ${__filename} port`);
+  } else {
+    let port = process.argv[2];
+    port = parseInt(port);
+    if (isNaN(port)) {
+      error("Escribe números");
+    } else {
+      console.log(port);
+      start(port);
+    }
+  }
+};
+
+main();
